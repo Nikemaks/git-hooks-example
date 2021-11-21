a1a6661c2164cd5c46c70c539ee9907f3b331f67
Maxim Malyi <malyi.1992@mail.ru>
2021-11-21 18:26:08 +0200

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+// Git Hook	Команда Git 	Описание
+// applypatch-msg.sample	git am	Когда сообщение патча изменено
+// commit-msg.sample	git commit	Чтобы включить сообщения о совершении действия (push)
+// fsmonitor-watchman.sample	launch watchman	Интеграция Watchman (служба просмотра файлов)
+// post-update.sample	git push	Обновление данных после отправки
+// pre-applypatch.sample	git am	Перед применением патча
+// pre-commit.sample	git commit	Перед публикацией (commit)
+// prepare-commit-msg.sample	git commit	Когда сообщение публикации (commit) установлено
+// pre-push.sample	git push	Прежде чем совершить действие (push)
+// pre-rebase.sample	git rebase	Прежде чем сделать передачу (pass) или слияние (merge)
+// pre-receive.sample	git push	Когда мы совершаем действие (push) и получаем данные из удалённого хранилища
+// update.sample	git push	Обновление удалённых данных одним действием (push).
