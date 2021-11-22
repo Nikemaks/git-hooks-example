3b920fe35fad373d8f3c209051b029f7ca0a61c6
Maksym Malyi <Maksym.Malyi@refinitiv.com>
2021-11-22 11:05:51 +0200

    test

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // commit-msg.sample	git commit	Чтобы включить сообщения о совершении действия (push)
 // fsmonitor-watchman.sample	launch watchman	Интеграция Watchman (служба просмотра файлов)
 // post-update.sample	git push	Обновление данных после отправки
+
 // pre-applypatch.sample	git am	Перед применением патча
 // pre-commit.sample	git commit	Перед публикацией (commit)
 // prepare-commit-msg.sample	git commit	Когда сообщение публикации (commit) установлено
